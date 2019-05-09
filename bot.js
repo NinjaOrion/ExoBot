a302f863bae2da506c84f7de5f4cab1e4ccfd642
NinjaOrion <david.gerber@live.de>
2019-05-09 18:03:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,7 +47,7 @@ client.on("message", async message => {
   // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
   // command = say
   // args = ["Is", "this", "the", "real", "life?"]
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const args = message.content.slice(process.env.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
   // Let's go with a few common example commands! Feel free to delete or change those.
