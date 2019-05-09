7e163cf4bfd8b3181df522613ab87e598a49eed9
Dave <david.gerber@live.de>
2019-05-09 14:11:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 // Here we load the config.json file that contains our token and our prefix values. 
-const config = require("./auth.json");
+//const config = require("./auth.json");
 // config.token contains the bot's token
 // config.prefix contains the message prefix.
 
