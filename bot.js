648c1760a9a9ba5f7888ead4130440594b30648b
NinjaOrion <david.gerber@live.de>
2019-05-14 10:08:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -103,7 +103,7 @@ client.on("message", async message => {
     // This command must be limited to mods and admins. In this example we just hardcode the role names.
     // Please read on Array.some() to understand this bit: 
     // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+    if(!message.member.hasPermission('KICK_MEMBERS') )
       return message.reply("Sorry, you don't have permissions to use this!");
     
     // Let's first check if we have a member and if we can kick them!
@@ -130,7 +130,7 @@ client.on("message", async message => {
   if(command === "ban") {
     // Most of this command is identical to kick, except that here we'll only let admins do it.
     // In the real world mods could ban too, but this is just an example, right? ;)
-    if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
+    if(!message.member.hasPermission('BAN_MEMBERS') )
       return message.reply("Sorry, you don't have permissions to use this!");
     
     let member = message.mentions.members.first();
