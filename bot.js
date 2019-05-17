d9157d67e5465b9b2726e427d59ff7bc2e56d3c9
NinjaOrion <david.gerber@live.de>
2019-05-17 16:35:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,19 +16,19 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setActivity(`on ${client.guilds.size} servers`);
+  client.user.setActivity(`on ${client.guilds.size} servers`, {type: "STREAMING"});
 });
 
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`on ${client.guilds.size} servers`);
+  client.user.setActivity(`on ${client.guilds.size} servers`, {type: "STREAMING"});
 });
 
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`on ${client.guilds.size} servers`);
+  client.user.setActivity(`on ${client.guilds.size} servers`, {type: "STREAMING"});
 });
 
 
